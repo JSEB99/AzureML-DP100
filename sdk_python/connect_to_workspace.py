5352fea97885f7c0399cc6d2be82d0d25ef1588c
Sebastian <sebastian.mt99@gmail.com>
2024-05-22 14:52:53 -0500

    execution fix

--- sdk_python/connect_to_workspace.py
+++ sdk_python/connect_to_workspace.py
@@ -11,9 +11,8 @@ resource_group = os.getenv("RESOURCE_GROUP")  # preferably using env variable
 workspace = os.getenv("WORKSPACE")  # preferably using env variable
 
 if workspace is None:
-    ml_client = MLClient(credential=DefaultAzureCredential(),
-                         subscription_id=subscription_id,
-                         resource_group_name=resource_group)
+    ml_client = MLClient(DefaultAzureCredential(),
+                         subscription_id, resource_group)
 else:
     ml_client = MLClient(
         DefaultAzureCredential(), subscription_id, resource_group, workspace
---
+++ sdk_python/create_workspace.py
@@ -0,0 +1,13 @@
+
+from azure.ai.ml.entities import Workspace
+from connect_to_workspace import ml_client
+
+workspace_name = "mlw-dp100"
+
+ws_basic = Workspace(
+    name=workspace_name,
+    location="eastus",
+    display_name="Basic workspace-example",
+    description="This example shows how to create a basic workspace",
+)
+ml_client.workspaces.begin_create(ws_basic)
